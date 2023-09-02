b1266a29d99f61fce749ca2f854b3448c11a80d4
lim1t <namest504@gmail.com>
2023-09-02 19:03:43 +0900

    init

---
+++ src/main/java/com/lim1t/preonboardingbreadcrumbs/PreOnBoardingBreadCrumbsApplication.java
@@ -0,0 +1,13 @@
+package com.lim1t.preonboardingbreadcrumbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreOnBoardingBreadCrumbsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PreOnBoardingBreadCrumbsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lim1t/preonboardingbreadcrumbs/PreOnBoardingBreadCrumbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lim1t.preonboardingbreadcrumbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreOnBoardingBreadCrumbsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
